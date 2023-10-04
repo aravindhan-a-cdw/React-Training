d6a11f5efe5e36eab2db94d0a98c9facbbe92625
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-10-04 13:30:02 +0530

    Changes based on review comments

--- users-list/src/components/UserCard/index.tsx
+++ users-list/src/components/UserCard/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./style.module.css";
+import Badge from "../Badge";
 
 type UserProps = {
 	data: {
@@ -13,17 +14,19 @@ function UserCard(props: UserProps) {
 	const {
 		data: { name, location, interests, image },
 	} = props;
+	const MAX_BADGES = 6;
+	const interestBadges = interests
+		.slice(0, MAX_BADGES)
+		.map((interest, index) => <Badge key={index}>{interest}</Badge>);
+	if (interests.length > MAX_BADGES)
+		interestBadges.push(<Badge key={MAX_BADGES}>...</Badge>);
 	return (
 		<div className={styles.user_card}>
 			<img src={image} alt="User Profile" />
 			<div className={styles.user_content}>
 				<span className={styles.user_name}>{name}</span>
 				<span className={styles.user_location}>{location}</span>
-				<ul className={styles.interests}>
-					{interests.map((interest, index) => (
-						<span key={index}>{interest}</span>
-					))}
-				</ul>
+				<ul className={styles.interests}>{interestBadges}</ul>
 			</div>
 		</div>
 	);
