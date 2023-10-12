bf7f5631f8e1ca54a18e0e3d827bb1c7f3e8c041
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-10-12 15:02:00 +0530

    Completed error page

--- explorer/src/pages/Error/index.tsx
+++ explorer/src/pages/Error/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./styles.module.scss";
+import { ERROR_CONSTANTS } from "../../constants/PageConstants";
+
 function Error() {
-	return <div>404 Not Found!</div>;
+	const [time, setTime] = useState(ERROR_CONSTANTS.TIME);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (time === 0) {
+				navigate("/");
+			}
+			setTime((time) => time - 1);
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, [time, navigate]);
+
+	return (
+		<div className={styles.container}>
+			<p>404 Not Found!</p>
+			<span>You will be redirected to home in {time} seconds!</span>
+		</div>
+	);
 }
 
 export default Error;
