114c4e3d44bc5440230e5862b6ceda1afcbac9ac
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-10-18 20:37:31 +0530

    Modified service and added comments in service

--- explorer/src/pages/Error/index.tsx
+++ explorer/src/pages/Error/index.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 import styles from "./styles.module.scss";
 import { ERROR_CONSTANTS } from "../../constants/PageConstants";
 
 function Error() {
 	const [time, setTime] = useState(ERROR_CONSTANTS.TIME);
 	const navigate = useNavigate();
+	const error = useRouteError() as Error;
+
+	console.log(error);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
@@ -21,7 +24,7 @@ function Error() {
 
 	return (
 		<div className={styles.container}>
-			<p>404 Not Found!</p>
+			<p>{error.message || "404 Not Found!"}</p>
 			<span>You will be redirected to home in {time} seconds!</span>
 		</div>
 	);
