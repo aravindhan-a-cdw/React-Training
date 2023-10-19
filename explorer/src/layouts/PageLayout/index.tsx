ee89a12beec41a9bad293854411729ddcf8ca3af
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-10-19 16:50:17 +0530

    Added loading state message

--- explorer/src/layouts/PageLayout/index.tsx
+++ explorer/src/layouts/PageLayout/index.tsx
@@ -1,14 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Nav from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
 function PageLayout() {
+	const { state } = useNavigation();
 	return (
 		<div>
 			<header>
 				<Nav />
 			</header>
-			<Outlet />
+			{state === "loading" ? "Loading" : <Outlet />}
 			<Footer />
 		</div>
 	);
