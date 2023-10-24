5e02ad839c471fd126628651df4742bf69d78e8a
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-10-24 22:50:08 +0530

    Orders Page

---
+++ sitback-furnitures/src/components/OrderItem/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.scss";
+
+type OrderData = {
+	description: string;
+	id: number;
+	name: string;
+	photo: string;
+	price: number;
+};
+
+type OrderItemProps = {
+	data: OrderData;
+	className?: string;
+};
+
+const OrderItem = (props: OrderItemProps) => {
+	const { className = "", data } = props;
+	return (
+		<div className={`${className} ${styles.order_item}`}>
+			<img src={data.photo} alt={data.name} />
+			<div className={styles.basic_info}>
+				<span>{data.name}</span>
+				<span>&#8377; {data.price}</span>
+			</div>
+			<p className={styles.quantity}>Quantity: {}</p>
+			<p className={styles.product_description}>{data.description}</p>
+		</div>
+	);
+};
+
+export default OrderItem;
--- sitback-furnitures/src/screens/Order/index.tsx
+++ sitback-furnitures/src/screens/Order/index.tsx
@@ -1,5 +1,52 @@
+import { useLoaderData } from "react-router-dom";
+import styles from "./styles.module.scss";
+import CategoriesDisplay from "../../components/CategoriesDisplay";
+import OrderItem from "../../components/OrderItem";
+import CardsContainer from "../../containers/CardsContainer";
+
+type CategoryData = {
+	id: string;
+	category: string;
+	photo: string;
+	description: string;
+};
+
 const Order = () => {
-	return <div>Orders Page</div>;
+	const data = useLoaderData() as Array<CategoryData>;
+	const orders = [
+		{
+			id: 1,
+			name: "3 Seater Yellow Sofa Set",
+			description: "Hello",
+			price: 1234,
+			photo: "https://images.unsplash.com/photo-1577451949326-1e44d745f758?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=1950&q=80",
+			quantity: 5,
+		},
+		{
+			id: 2,
+			name: "2 Seater Sofa",
+			description: "Hello",
+			price: 12999,
+			photo: "https://images.unsplash.com/photo-1558211583-d26f610c1eb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1895&q=80",
+			quantity: 2,
+		},
+	];
+	const orderItems = orders.map((data) => <OrderItem data={data} />);
+	return (
+		<>
+			<div className={styles.orders_container}>
+				<h3>Order Confirmation</h3>
+				<p>
+					Thank you for shopping with us. The items will be delivered
+					within 7 days.
+				</p>
+				<CardsContainer className={styles.orders_cards}>
+					{orderItems}
+				</CardsContainer>
+			</div>
+			<CategoriesDisplay categories={data} />
+		</>
+	);
 };
 
 export default Order;
