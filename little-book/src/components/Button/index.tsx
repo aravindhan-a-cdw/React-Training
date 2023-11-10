714033ca5a23d99f50d841eeedae5ef8da2dafcd
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-11-10 15:25:56 +0530

    Added variants in button

--- little-book/src/components/Button/index.tsx
+++ little-book/src/components/Button/index.tsx
@@ -1,10 +1,18 @@
+import styles from "./styles.module.scss";
+
+/*
+	@author Aravindhan A
+	@description This component renders the button with variants primary and secondary
+*/
+
 type ButtonProps = {
 	children: React.ReactNode;
+	type?: "primary" | "secondary";
 };
 
 const Button = (props: ButtonProps) => {
-	const { children } = props;
-	return <button>{children}</button>;
+	const { children, type = "primary" } = props;
+	return <button className={styles[type]}>{children}</button>;
 };
 
 export default Button;
