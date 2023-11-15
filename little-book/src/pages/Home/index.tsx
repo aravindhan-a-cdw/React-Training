7bcf6f9556824e5f080b441da05994813ab9ff93
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-11-15 12:34:28 +0530

    Added Home Page

---
+++ little-book/src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import HomeLayout from "../../containers/HomeLayout";
+
+const Home = () => {
+	return <HomeLayout />;
+	// return (
+	// 	<div>
+	// 		<h1>Home</h1>
+	// 		<Button>New</Button>
+	// 		<CheckBox />
+	// 		<SearchBar placeholder="Search Blogs" />
+	// 		<BlogSummary
+	// 			details="fasdfkjasjkfkaskjf"
+	// 			photo=""
+	// 			title="How to Time Travel"
+	// 			type="International"
+	// 		/>
+	// 	</div>
+	// );
+};
+
+export default Home;
