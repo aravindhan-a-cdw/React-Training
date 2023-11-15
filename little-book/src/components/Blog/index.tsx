b23e39ba9413bfcd8c520eb3ae53c691fe6e7864
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-11-15 13:18:07 +0530

    Added fallback image

--- little-book/src/components/Blog/index.tsx
+++ little-book/src/components/Blog/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import Button from "../Button";
 import { useSelector } from "react-redux";
 import { selectBlogs, selectSelectedBlog } from "../../actions/blog";
+import fallbackImage from "../../assets/default-fallback-image.png";
 
 type BlogProps = {
 	className?: string;
@@ -18,14 +19,18 @@ const Blog = (props: BlogProps) => {
 	const blogs: Array<BlogType> = useSelector(selectBlogs);
 	const selectedBlog = useSelector(selectSelectedBlog);
 
-	const blogDetails = blogs[selectedBlog];
+	const blogDetails = blogs[selectedBlog] || {};
 
 	const { className = "" } = props;
 
-	const { photo, title, details } = blogDetails;
+	const { photo = "", title, details } = blogDetails;
 	return (
 		<div className={`${styles.blogContainer} ${className}`}>
-			<img src={photo} alt={title} />
+			<img
+				onError={(event) => (event.currentTarget.src = fallbackImage)}
+				src={photo}
+				alt={title}
+			/>
 			<h2>{title}</h2>
 			<p>{details}</p>
 			<Button className={styles.button} type="secondary">
