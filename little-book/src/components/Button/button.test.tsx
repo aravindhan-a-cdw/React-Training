910087cde591b603293ed1524626672298d20df0
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-11-22 12:08:54 +0530

    Moved test file

--- little-book/src/components/Button/button.test.tsx
+++ little-book/src/components/Button/button.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import Button from "../../components/Button";
+import Button from ".";
 import "@testing-library/jest-dom";
 
 describe("Button Renders text", () => {
-	it("Hello is rendered in the screen", () => {
+	it("Hello is rendered in the screen by button", () => {
 		render(<Button>Hello</Button>);
 		const element = screen.getByText("Hello");
 		expect(element).toBeInTheDocument();
